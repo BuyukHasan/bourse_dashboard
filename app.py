29f043cbdcf106d13970803f9f291e2200864879
BuyukHasan <buyukh7723@gmail.com>
2025-07-26 14:44:31 +0200

    data_update
    add class DataFetcher with methods :
    -fetch_data
    -_clean_data
    add class TechnicalAnalyzer with methods :
    -show_dataframe
    -calcul_50_200_jours
    -add_rsi

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from src.data_fetcher import DataFetcher
+from src.technical_analyzer import TechnicalAnalyzer
 
 fetcher = DataFetcher("TSLA")
 df = fetcher.fetch_data()
-print(df)
+
+analyze = TechnicalAnalyzer(df)
+analyze.calcul_50_200_jours()
+analyze.show_dataframe()
---
+++ src/technical_analyzer.py
@@ -0,0 +1,35 @@
+from data_fetcher import DataFetcher
+import pandas as pd
+import numpy as np
+
+class TechnicalAnalyzer :
+
+    def __init__(self, data_frame):
+        # Vérification des colonnes nécessaires
+        required_columns = ['Ouv', 'Haut', 'Bas', 'Clôt']
+        if not all(col in data_frame.columns for col in required_columns):
+            raise ValueError("Il manque des colonnes nécessaires")
+        
+        self.df = data_frame
+    
+    def show_dataframe(self):
+        return self.df
+    def calcul_50_200_jours(self):
+        # Vérifie que la colonne Clôt existe
+        if 'Clôt' not in self.df.columns:
+            raise ValueError("La colonne 'Clôt' est manquante")
+        
+        # Calcul des moyennes mobiles
+        self.df['MA_50'] = self.df['Clôt'].rolling(window=50).mean()
+        self.df['MA_200'] = self.df['Clôt'].rolling(window=200).mean()
+    def add_rsi(self , window = 14) :
+
+        delta = self.df['Clôt'].diff()
+
+        gain = delta.where(delta > 0.0 , 0)
+        loss = -delta.where(delta < 0.0 , 0)
+
+        avg_gain = gain.rolling(window).mean()
+        avg_loss = loss.rolling(window).mean()
+
+        self.df['rsi'] = 100 - (100 / (1 + (avg_gain / avg_loss.replace(0, 1))))
